a2a62b9267e3110039ca6cff975055af37638658
bayram <bayram@adcreative.com>
2025-01-22 03:39:21 +0300

    asd

--- EfCoreDeleteExample/Controllers/ProductController2.cs
+++ EfCoreDeleteExample/Controllers/ProductController2.cs
@@ -9,7 +9,6 @@ namespace EfCoreDeleteExample.Controllers;
 public class ProductController2 : ControllerBase
 {
     private readonly AppDbContext _context;
-asfsafsfas
     public ProductController2(AppDbContext context)
     {
         _context = context;
