e672dd218c1e3ed3fecb88be29895718fd91aa4e
bayram <bayram@adcreative.com>
2025-01-24 02:36:17 +0300

    s

--- EfCoreDeleteExample/Controllers/ProductController4.cs
+++ EfCoreDeleteExample/Controllers/ProductController4.cs
@@ -20,8 +20,6 @@ public class ProductController4 : ControllerBase
         var result = await _context.Products.DeleteByIdAsync(id);
         if (result)
         {
-            as
-            await _context.SaveChangesAsync();
             return Ok($"Product with id {id} deleted successfully");
         }
         return NotFound($"Product with id {id} not found");
