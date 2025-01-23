b043b74b709f6a96f0b47e2556c0ebd4f9869632
bayram <bayram@adcreative.com>
2025-01-24 02:12:46 +0300

    as

--- EfCoreDeleteExample/Controllers/ProductController4.cs
+++ EfCoreDeleteExample/Controllers/ProductController4.cs
@@ -13,7 +13,6 @@ public class ProductController4 : ControllerBase
     {
         _context = context;
     }
-
     
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteById(int id)
